c8939c289d9305b39e713735ebad90cb4c8d95f5
angelogosim-stack <angelogosim954@gmail.com>
2026-05-20 23:51:58 +0800

    Update _05_view_book.py

--- _05_view_book.py
+++ _05_view_book.py
@@ -1,2 +1,19 @@
 def view_books(self) -> list:
     return list(self.books.values())
+
+# usage in main python 
+
+books = service.view_books()
+
+if not books:
+    print("No books found.")
+
+else:
+    for book in books:
+
+        status = "Available" if book.is_available else "Borrowed"
+
+        print(
+            f"[{book.book_id}] "
+            f"{book.title} by {book.author} - {status}"
+        )
